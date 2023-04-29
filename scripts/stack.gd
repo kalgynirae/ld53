edd61ece1982b82d46346274b27d27fa4653a2fa
Colin Chan <colin+git@lumeh.org>
2023-04-28 20:56:42 -0700

    Reorganize all the files because Sophia said so

---
+++ scripts/stack.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+class_name Stack
+
+var items: Array[Node]
+
+func _ready():
+	for item in get_children():
+		if item.name != "Base":
+			items.push_back(item)
+	arrange()
+
+func activate():
+	$Base.visible = true
+
+func deactivate():
+	$Base.visible = false
+
+func push(item: Node):
+	items.push_back(item)
+	arrange()
+
+func pop() -> Node:
+	var item = items.pop_back()
+	arrange()
+	return item
+
+func arrange():
+	for i in items.size():
+		items[i].position.y = (i + 1) * .05
